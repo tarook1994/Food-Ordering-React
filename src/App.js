95f5cab56e8e80fe1e28c215051223c86efbd68d
Ahmed Tarek <ahmed-tarek94@hotmail.com>
2019-06-06 01:19:49 +0200

    Added login

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import MainPage from './Containers/MainPage/MainPage'
+import Login from './Components/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      <MainPage/>
+      {/* <MainPage/> */}
+      <Login/>
     </div>
   );
 }
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Login.css'
+
+class Login extends React.Component {
+
+    render() {
+        return (
+            <div className='login-container'>
+
+            </div>
+        )
+    }
+}
+export default Login;
